34cd1fa93ef25809ba6e493726a3c1ba818567dc
Jacob Joseph Jorishie <jake.jorishie@Jacobs-MBP.attlocal.net>
2022-08-06 12:25:51 -0500

    changed client

--- db/index.js
+++ db/index.js
@@ -1,6 +1,11 @@
 const { Client } = require('pg'); 
 
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
+const client = new Client({
+	connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+	ssl: {
+	  rejectUnauthorized: false
+	}
+  });
 
 const getAllUsers = async () => {
 	try {
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 require('dotenv').config();
-
 const { PORT = 3000 } = process.env
 const express = require('express');
 const server = express();
-
 const morgan = require('morgan');
 server.use(morgan('dev'));
 
